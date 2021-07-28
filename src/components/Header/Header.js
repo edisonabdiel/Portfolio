740d9e7c8f606220b74973fa30efbec32384e4de
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-07-28 21:18:22 +0200

    feat: Main application set up. Styled Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillFacebook, AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { GiBoltEye } from 'react-icons/gi';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () => (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+          <GiBoltEye size='3rem' /><span>EdisonAbdiel</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/edisonabdiel">
+        <AiFillGithub size='2.5rem' />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/edison-abdiel-32467b1a7/">
+        <AiFillLinkedin size='2.5rem' />
+      </SocialIcons>
+      <SocialIcons href="https://www.facebook.com/edison.abdiel">
+        <AiFillFacebook size='2.5rem' />
+      </SocialIcons>
+    </Div3>
+  </Container>
+);
+
+export default Header;
